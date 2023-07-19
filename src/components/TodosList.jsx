c8e95982a553c23651d63267422bd15fa56a898e
ctokoli <matumasie@gamil.com>
2023-07-19 18:00:11 +0000

    Update TodosList.jsx

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,17 +1,23 @@
 import TodoItem from '@/components/TodoItem';
 import PropTypes from 'prop-types';
 
-const TodosList = ({ todosProps, setTodos }) => {
+const TodosList = ({ todosProps, handleChange, delTodo}) => {
     return ( 
         <ul>
             {todosProps.map((todo) => (
-               <TodoItem key={todo.id} todoItem={todo} setTodos={setTodos}/>
+               <TodoItem 
+                key={todo.id} 
+                todoItem={todo} 
+                handleChange={handleChange}
+                delTodo={delTodo}
+                />
             ))}
         </ul>
      );
 }
  TodosList.propTypes = {
   todosProps: PropTypes.array.isRequired,
-  setTodos: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
 };
 export default TodosList;
