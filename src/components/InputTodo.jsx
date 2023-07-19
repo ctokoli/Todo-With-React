04194278e3649ebfa636f8b6ca77be999065b480
ctokoli <matumasie@gamil.com>
2023-07-19 23:48:23 +0000

    Update InputTodo.jsx

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
+
 const InputTodo = () => {
+   const [title, setTitle] = useState('');
+
+   const handleChange = (e) => {
+    setTitle(e.target.value)
+    
+   }
     return ( 
-        <div>
-            Input field here.....
-        </div>
-     );
+        <form>
+            <input type="input" 
+             value={title} 
+             placeholder="Add Todo...." 
+             onChange={handleChange}   
+             />
+            <button>Submit</button>
+        </form>
+    )
 }
  
 export default InputTodo;
