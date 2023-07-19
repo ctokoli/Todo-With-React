cfb1a9d4834aea9db9a0178f8b7ae69d32611306
ctokoli <matumasie@gamil.com>
2023-07-19 17:05:42 +0000

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,33 @@
+import PropTypes from 'prop-types';
+
 const TodoItem = ({todoItem, setTodos}) => {
-    
+    const handleChange = (id) => {
+      setTodos((prevState) => 
+        prevState.map((todo) => {
+           if(todo.id === id){
+             return {
+                ...todo,
+                completed: !todo.completed
+            };
+           }
+           return todo;
+        })
+      )
+    }
     return (
     <li>
         <input 
             type="checkbox" 
             checked={todoItem.completed}
-            onChange={ () => console.log('clicked')}
+            onChange={ () => handleChange(todoItem.id)}
         />
         {todoItem.title}
     </li>
     );
 }
  
+TodoItem.propTypes = {
+  todoItem: PropTypes.string.isRequired,
+  setTodos: PropTypes.func.isRequired,
+};
 export default TodoItem;
